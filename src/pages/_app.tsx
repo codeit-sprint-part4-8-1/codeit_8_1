3165ea6bea500199897800630b9ff371192f5a29
jaewanLee <wodhks3791@naver.com>
2024-10-12 07:22:44 +0900

    F81-14 feat: 상단 네비게이션바 제작

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import GNB from '@/components/@Shared/GNB';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GNB />
+      <Component {...pageProps} />;
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,3 @@
-
 export default function Home() {
-  return (
-    <div className="bg-nomadBlack">홈</div>
-  );
+  return <div className="bg-nomadBlack">메인 페이지</div>;
 }
