6cf634dcff16800774a9d5f09d0c38c85bf664e5
yamdang <youm4000@naver.com>
2024-10-13 17:35:57 +0900

    test: 드롭다운 컴포넌트 테스트 코드 삭제 #F81-29

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,3 @@
-import DropDownMenu from '@/components/@Shared/dropDown/DropDownMenu';
-
 export default function Home() {
-  return (
-    <div className="bg-nomadBlack">
-      홈{' '}
-      <DropDownMenu
-        size={'large'}
-        filterList={[
-          '전체',
-          '예약 신청',
-          '예약 취소',
-          '예약 승인',
-          '예약 거절',
-          '체험 완료',
-        ]}
-      />
-    </div>
-  );
+  return <div className="bg-nomadBlack">홈</div>;
 }
