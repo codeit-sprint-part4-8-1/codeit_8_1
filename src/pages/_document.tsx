9d61abe64114f91b5f0d3a26daeb659f603a11f9
yamdang <youm4000@naver.com>
2024-10-18 22:04:32 +0900

    feat: 전체 페이지 배경 색상 설정 #F81-12

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,10 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="antialiased">
+      <body className="antialiased bg-gray-100">
         <Main />
         <NextScript />
       </body>
