3d06efa216c5772f01872ed7ed551dacdf643c3b
yamdang <youm4000@naver.com>
2024-10-22 21:57:52 +0900

    feat: 무한 스크롤 쓰로틀링 방식 적용 #F81-13

--- src/hook/useInfiniteScroll.tsx
+++ src/hook/useInfiniteScroll.tsx
@@ -11,14 +11,19 @@ export default function useInfiniteScroll({
   setHasMore,
   setItems,
 }: useInfiniteScrollProps) {
-  // 디바운스 함수 - 불필요한 스크롤 이벤트 호출 방지
-  const debounce = (func: any, delay: number) => {
-    let timeoutId: any;
-    return (...args: any[]) => {
-      if (timeoutId) clearTimeout(timeoutId);
-      timeoutId = setTimeout(() => {
-        func.apply(null, args);
-      }, delay);
+  // 쓰로틀링 함수 - 불필요한 스크롤 이벤트 호출 방지(함수를 실행후 일정 시간 지난후 실행을 반복 일정 시간동안은 실행을 무시한다.)
+  const throttle = (callback: any, limit: number) => {
+    let isThrottle: boolean;
+
+    return () => {
+      if (!isThrottle) {
+        callback(); // callback 함수 실행
+        isThrottle = true; // 실행후 바로 실행 안되도록 적용
+
+        setTimeout(() => {
+          isThrottle = false; // limit로 지정한 시간 뒤에 실행 되도록 적용
+        }, limit);
+      }
     };
   };
 
@@ -27,14 +32,16 @@ export default function useInfiniteScroll({
     const scrollHeight = document.documentElement.scrollHeight; // 전체 문서 높이
     const clientHeight = window.innerHeight; // 현재 화면에 보여지는 영역의 높이
 
+    const scrollPercentage = (scrollTop + clientHeight) / scrollHeight;
+
     // 현재 스크롤 위치가 컨텐츠의 마지막에 가까운지 확인
-    if (scrollHeight - clientHeight - scrollTop < 150 && hasMore) {
+    if (scrollPercentage > 0.8 && hasMore) {
       loadMoreItems();
     }
   };
 
   // 스크롤 이벤트 발생 후 500ms동안 스크롤을 하지 않으면 handleScroll함수 호출
-  const debouncedHandleScroll = debounce(handleScroll, 300);
+  const throttledHandleScroll = throttle(handleScroll, 300);
 
   // 데이터 추가 로드 함수
   const loadMoreItems = () => {
@@ -54,7 +61,7 @@ export default function useInfiniteScroll({
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', debouncedHandleScroll);
-    return () => window.removeEventListener('scroll', debouncedHandleScroll);
+    window.addEventListener('scroll', throttledHandleScroll);
+    return () => window.removeEventListener('scroll', throttledHandleScroll);
   }, [hasMore]);
 }
