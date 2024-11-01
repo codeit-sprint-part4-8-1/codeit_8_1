d777e60bd113d4739de1ff4d7bc295b5ab44e9e1
yamdang <youm4000@naver.com>
2024-11-01 22:40:38 +0900

    fix: 임시 로그인 기능 삭제 #F81-13

--- src/pages/myInfo/index.tsx
+++ src/pages/myInfo/index.tsx
@@ -1,4 +1,4 @@
-import { fetchLoginTest, fetchUserInfo } from '@/apis/myInfo/api';
+import { fetchUserInfo } from '@/apis/myInfo/api';
 import ProfileMenu from '@/components/@Shared/profileMenu/ProfileMenu';
 import MyInfoForm from '@/components/myInfo/MyInfoForm';
 import { UserInfo } from '@/types/myPage/type';
@@ -9,22 +9,6 @@ import useUserInfo from '@/hook/useUserInfo';
 
 export default function MyInfo() {
   const { data, isLoading } = useUserInfo();
-
-  // 로그인 기능구현전이어서 임시로 구현
-  const getLoginTest = async () => {
-    try {
-      const userData = await fetchLoginTest({
-        userEmail: 'youm96@naver.com',
-        userPassword: '12341234',
-      });
-    } catch (error) {
-      console.error('Login failed:', error);
-    }
-  };
-
-  useEffect(() => {
-    getLoginTest();
-  }, []);
 
   if (isLoading) {
     return <div>로딩중...</div>;
