2aaeb4c189500af714e6f20b5142ea4e937c4bb5
yamdang <youm4000@naver.com>
2024-11-02 20:56:19 +0900

    feat: 에러 바운더리 영역 수정 #F81-13

--- src/pages/myInfo/index.tsx
+++ src/pages/myInfo/index.tsx
@@ -4,7 +4,7 @@ import { ErrorBoundary } from 'react-error-boundary';
 import useUserInfo from '@/hook/useUserInfo';
 import LoadingSpinner from '@/components/@Shared/loading/LoadingSpinner';
 
-export default function MyInfo() {
+const MyInfoContent = () => {
   const { data, isLoading } = useUserInfo();
 
   if (isLoading) {
@@ -12,11 +12,17 @@ export default function MyInfo() {
   }
 
   return (
-    <ErrorBoundary fallback={<div>에러</div>}>
-      <div className="flex justify-center w-full gap-6 mt-20 mb-20">
-        <ProfileMenu profileImageUrl={data?.profileImageUrl} />
-        <MyInfoForm nickname={data?.nickname} email={data?.email} />
-      </div>
+    <div className="flex justify-center w-full gap-6 mt-20 mb-20">
+      <ProfileMenu profileImageUrl={data?.profileImageUrl} />
+      <MyInfoForm nickname={data?.nickname} email={data?.email} />
+    </div>
+  );
+};
+
+export default function MyInfo() {
+  return (
+    <ErrorBoundary fallback={<div>에러가 발생했습니다.</div>}>
+      <MyInfoContent />
     </ErrorBoundary>
   );
 }
