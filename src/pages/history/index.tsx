0d023ad8b1a20fbe5f557e570073b4eb0eb4af94
yamdang <youm4000@naver.com>
2024-11-02 20:59:11 +0900

    feat: 조건에 따른 UI 노출을 ts-pattern으로 적용 #F81-13

--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -1,5 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
-import { Button } from '@/components/@Shared/Buttons/Button';
+import { useState, useRef } from 'react';
 import DropDownMenu from '@/components/@Shared/dropDown/DropDownMenu';
 import ProfileMenu from '@/components/@Shared/profileMenu/ProfileMenu';
 import ConfirmModal from '@/components/@Shared/modal/ConfirmModal';
@@ -7,13 +6,15 @@ import ModalFrame from '@/components/@Shared/modal/ModalFrame';
 import useUserInfo from '@/hook/useUserInfo';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { fetchReservationList } from '@/apis/myInfo/api';
-import Image from 'next/image';
 import ReviewModal from '@/components/@Shared/modal/ReviewModal';
 import { Toaster } from 'react-hot-toast';
 import useObserverScroll from '@/hook/useObserverScroll';
 import NotData from '@/components/history/NotData';
 import { reservationValidation } from '@/utils/reservationValidation';
 import LoadingSpinner from '@/components/@Shared/loading/LoadingSpinner';
+import ReservationCard from '@/components/history/ReservationCard';
+import { match } from 'ts-pattern';
+import ReservationSkeletonCard from '@/components/history/ReservationSkeletonCard';
 
 export default function History() {
   const MENU_LIST = [
@@ -51,7 +52,6 @@ export default function History() {
   const [reservationId, setReservationId] = useState<number>();
   const [filterStatus, setFilterStatus] = useState<string | null>(null);
   const loadMoreRef = useRef(null);
-  console.log(filterStatus);
 
   const {
     data: resDataList,
@@ -67,7 +67,6 @@ export default function History() {
         size: 10,
         status: filterStatus,
       });
-      console.log(res);
       return res;
     },
     getNextPageParam: (lastPage) => {
@@ -88,6 +87,13 @@ export default function History() {
   const handleCancelClick = (id: number) => {
     setReservationId(id);
     setConfirmModalOpen(true);
+  };
+
+  // 후기 작성
+  const handleReviewClick = (res: any) => {
+    setIsOpen(true);
+    setReviewModalOpen(true);
+    setReviewData(res);
   };
 
   if (isLoading && status === 'pending') {
@@ -116,75 +122,35 @@ export default function History() {
           />
         </div>
         <div>
-          {resDataList ? (
-            resDataList.pages
-              .flatMap((page) => page.reservations)
-              .map((res: any) => {
-                const { statusText, statusColor } = reservationValidation(
-                  res.status,
-                );
-                return (
-                  <div
-                    key={res.id}
-                    className="flex rounded-3xl overflow-hidden border-2 border-gray-100 bg-white mb-6 shadow-[0px_0px_10px_rgba(0,0,0,0.1)]"
-                  >
-                    <div
-                      className="w-52 h-52 bg-cover bg-center bg-no-repeat flex-shrink-0 text-[0px]"
-                      style={{
-                        backgroundImage: `url(${
-                          res.activity.bannerImageUrl ||
-                          '/image/defaultImage.png'
-                        })`,
-                      }}
-                    >
-                      이미지 영역
-                    </div>
-                    <div className="flex flex-col justify-center gap-3 w-full p-6">
-                      <span className={`text-base ${statusColor}`}>
-                        {statusText}
-                      </span>
-                      <h3 className="text-xl font-bold">
-                        {res.activity.title}
-                      </h3>
-                      <span className="text-lg">
-                        {res.date} ・ {res.startTime} - {res.endTime}{' '}
-                        {res.headCount}명
-                      </span>
-                      <div className="flex justify-between">
-                        <span className="text-2xl">₩10000</span>
-                        {statusText === '예약 완료' && (
-                          <>
-                            <Button
-                              label="예약 취소"
-                              variant="line"
-                              className="w-36 h-11"
-                              onClick={() => {
-                                handleCancelClick(res.id);
-                              }}
-                            />
-                          </>
-                        )}
-                        {statusText === '체험 완료' &&
-                          res.reviewSubmitted === false && (
-                            <Button
-                              label="후기 작성"
-                              variant="solid"
-                              className="w-36 h-11"
-                              onClick={() => {
-                                setIsOpen(true);
-                                setReviewModalOpen(true);
-                                setReviewData(res);
-                              }}
-                            />
-                          )}
-                      </div>
-                    </div>
-                  </div>
-                );
-              })
-          ) : (
-            <NotData />
-          )}
+          {match(status)
+            .with('pending', () =>
+              Array.from({ length: 3 }, (_, index) => (
+                <ReservationSkeletonCard key={index} />
+              )),
+            )
+            .with('error', () => <NotData status="error" />)
+            .with('success', () => {
+              if (resDataList) {
+                return resDataList.pages
+                  .flatMap((page) => page.reservations)
+                  .map((res: any) => {
+                    const statusValue = reservationValidation(res.status);
+                    return (
+                      <ReservationCard
+                        key={res.id}
+                        res={res}
+                        statusValue={statusValue}
+                        handleCancelClick={handleCancelClick}
+                        handleReviewClick={handleReviewClick}
+                      />
+                    );
+                  });
+              }
+              return <NotData status="not" />;
+            })
+            .otherwise(() => (
+              <NotData status="not" />
+            ))}
           {isFetchingNextPage && (
             <p className="text-center mt-4">데이터를 가져오고 있습니다.</p>
           )}
