a90e1befa6c6691eadad637254b53a3e8567c62c
yamdang <youm4000@naver.com>
2024-11-02 20:59:57 +0900

    feat: 데이터가 없는경우와 데이터를 불러오지 못한 경우 추가 #F81-13

--- src/components/history/NotData.tsx
+++ src/components/history/NotData.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image';
 
-export default function NotData() {
+interface NotData {
+  status: string;
+}
+
+export default function NotData({ status }: NotData) {
   return (
     <div className="flex flex-col justify-center items-center gap-12 mt-28">
       <Image
@@ -9,7 +13,11 @@ export default function NotData() {
         height={177}
         alt="데이터 없는경우 이미지"
       />
-      <p className="text-2xl text-gray-900">아직 예약한 체험이 없어요.</p>
+      <p className="text-2xl text-gray-900">
+        {status === 'not'
+          ? '아직 예약한 체험이 없어요.'
+          : '데이터를 불러어는데 실패했습니다.'}
+      </p>
     </div>
   );
 }
