fb9fc391db8b0dc76d16ae9fef7f64a1bbcfb1c9
yamdang <youm4000@naver.com>
2024-11-04 22:17:53 +0900

    feat: 한 페이지에서 클릭된 버튼에따라 해당 컴포넌트가 보여지도록 구현 #F81-13

--- src/pages/myInfo/index.tsx
+++ src/pages/myInfo/index.tsx
@@ -3,18 +3,76 @@ import MyInfoForm from '@/components/myInfo/MyInfoForm';
 import { ErrorBoundary } from 'react-error-boundary';
 import useUserInfo from '@/hook/useUserInfo';
 import LoadingSpinner from '@/components/@Shared/loading/LoadingSpinner';
+import MoContainer from '@/components/@Shared/MoContainer';
+import { useEffect, useState } from 'react';
+import ReservationList from '@/components/history/ReservationList';
+import { match } from 'ts-pattern';
 
 const MyInfoContent = () => {
   const { data, isLoading } = useUserInfo();
+  const [menuValue, setMenuValue] = useState<string>('내 정보');
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    const windowResize = () => {
+      if (window.innerWidth > 767) {
+        setIsVisible(true);
+      }
+    };
+
+    windowResize();
+
+    window.addEventListener('resize', windowResize);
+
+    return () => {
+      window.removeEventListener('resize', windowResize);
+    };
+  }, []);
+
+  const handleMenuClick = (value: string) => {
+    setMenuValue(value);
+    if (window.innerWidth <= 767) {
+      setIsVisible(true);
+    }
+  };
 
   if (isLoading) {
     return <LoadingSpinner />;
   }
 
   return (
-    <div className="flex justify-center w-full gap-6 mt-20 mb-20">
-      <ProfileMenu profileImageUrl={data?.profileImageUrl} />
-      <MyInfoForm nickname={data?.nickname} email={data?.email} />
+    <div className="flex justify-center w-full gap-6 mt-10 sm:mt-20 mb-10 sm:mb-20">
+      <ProfileMenu
+        profileImageUrl={data?.profileImageUrl}
+        menuValue={menuValue}
+        onHandleMenuClick={handleMenuClick}
+        isVisible={isVisible}
+      />
+
+      {match(menuValue)
+        .with('내 정보', () => (
+          <MoContainer isVisible={isVisible}>
+            <MyInfoForm
+              nickname={data?.nickname}
+              email={data?.email}
+              setIsVisible={setIsVisible}
+            />
+          </MoContainer>
+        ))
+        .with('예약 내역', () => (
+          <MoContainer isVisible={isVisible}>
+            <ReservationList setIsVisible={setIsVisible} />
+          </MoContainer>
+        ))
+        .otherwise(() => (
+          <MoContainer isVisible={isVisible}>
+            <MyInfoForm
+              nickname={data?.nickname}
+              email={data?.email}
+              setIsVisible={setIsVisible}
+            />
+          </MoContainer>
+        ))}
     </div>
   );
 };
