e55124066459b2a100b47f57748950afb508aef7
jaewanLee <wodhks3791@naver.com>
2024-11-04 12:09:25 +0900

    feat: F81-41 인기 체험 카드와 목록 제작

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import Banner from '@/components/main/Banner';
 import SearchBar from '@/components/@Shared/searchBar/SearchBar';
+import HotActivityList from '@/components/mainPage/HotActivityList';
 
 export default function Home() {
   return (
-    <div className="relative w-full h-full">
+    <div className="relative w-full">
       {/* <div className="h-240 md:h-550 bg-gray-800 flex items-center justify-center">
         <ul>
           <li>
@@ -13,11 +14,14 @@ export default function Home() {
           </li>
         </ul>
       </div> */}
-      <div className="absolute top-0 left-0 z-10 w-full h-full">
+      <div className="relative z-10">
         <Banner />
       </div>
       <div className="absolute top-[180px] sm:top-[490px] left-1/2 transform -translate-x-1/2 z-20">
         <SearchBar />
+      </div>
+      <div className="relative mobile:mt-[100px] tablet2:mt-[142px] pc:mt-[158px] z-30 flex justify-center">
+        <HotActivityList />
       </div>
       <div className="text-center py-8">
         <p>메인 페이지 내용</p>
