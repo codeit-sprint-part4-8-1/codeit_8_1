7b9e74941c476ccf872f5a70dc5176e55e933b2d
Minjo Kwawk <iclock1960@naver.com>
2024-11-05 18:35:37 +0900

    app 에 RootProvider import 후 감싸기

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import GNB from '@/components/@Shared/GNB';
 import Container from '@/components/@Shared/Container';
 import Footer from '@/components/@Shared/Footer';
+import { RootProvider } from '@/hook/useRoot';
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -19,14 +20,16 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <div className="flex flex-col min-h-screen">
-      <GNB />
-      {/* 컨테이너 컴포넌트에 fullWidth prop을 전달 */}
-      <Container fullWidth={isMainPage}>
-        <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
-        </QueryClientProvider>
-      </Container>
-      <Footer className="mt-auto" />
+      <RootProvider>
+        <GNB />
+        {/* 컨테이너 컴포넌트에 fullWidth prop을 전달 */}
+        <Container fullWidth={isMainPage}>
+          <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+          </QueryClientProvider>
+        </Container>
+        <Footer className="mt-auto" />
+      </RootProvider>
     </div>
   );
 }
