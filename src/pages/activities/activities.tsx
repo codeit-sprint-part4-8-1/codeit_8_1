b6f601acbd19d5da125c726bdd945cad76cb7d47
yamdang <youm4000@naver.com>
2024-11-10 00:14:16 +0900

    fix: import 파일명 대소문자 오타 수정 #F81-63

--- src/pages/activities/activities.tsx
+++ src/pages/activities/activities.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Category from '@/components/@Shared/dropdown/Dropdown';
+import Category from '@/components/@Shared/dropDown/Dropdown';
 import axios from 'axios';
 
 const ExperienceForm = () => {
