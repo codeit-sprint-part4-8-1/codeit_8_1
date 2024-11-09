ffa71f980207ffe20db459a0ab80c5d1d4d1df71
jaewanLee <wodhks3791@naver.com>
2024-11-09 11:51:57 +0900

    F81-75 fix: 카테고리명 수정

--- src/components/@Shared/dropdown/Dropdown.tsx
+++ src/components/@Shared/dropdown/Dropdown.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { HiChevronDown } from 'react-icons/hi';
 
-const categories = ['문화 예술', '식음료', '스포츠', '투어', '관광'];
+const categories = ['문화 ・ 예술', '식음료', '스포츠', '투어', '관광'];
 
 interface DropdownProps {
   selectedCategory: string;
